238ad09effce9d309ff322454aa3d55e93833ea7
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-29 11:23:04 +0300

    Add mission.js with basic structure

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,9 +1,27 @@
-function Mission() {
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const Mission = ({ missionName, missionDescription, highlights }) => {
+  const rows = classNames('border pl-2', {
+    'bg-gray-100':highlights,
+  });
   return (
-    <div>
-      <h2>Mission Page</h2>
-    </div>
+    <>
+      <tr className={rows}>
+        <td className='border pl-5 pt-5 align-top'>{ missionName }</td>
+        <td className='border p-5 text-justify'>{ missionDescription }</td>
+        <td className='border pl-5'>Not a Member</td>
+        <td className='border pl-5' aria-hidden='true' />
+      </tr>
+    </>
   );
-}
+};
 
 export default Mission;
+
+Mission.PropTypes = {
+  missionName: PropTypes.string.isRequired,
+  missionDescription: PropTypes.string.isRequired,
+  highlights: PropTypes.bool.isRequired,
+};
+
