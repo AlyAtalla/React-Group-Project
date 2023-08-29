78f9dc5b964cd4ac0cdea57559831dac76a2845c
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-29 11:35:25 +0300

    Fix linter errors

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,26 +1,25 @@
+import React from 'react'; // Don't forget to import React
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const Mission = ({ missionName, missionDescription, highlights }) => {
   const rows = classNames('border pl-2', {
-    'bg-gray-100':highlights,
+    'bg-gray-100': highlights,
   });
   return (
-    <>
-      <tr className={rows}>
-        <td className='border pl-5 pt-5 align-top'>{ missionName }</td>
-        <td className='border p-5 text-justify'>{ missionDescription }</td>
-        <td className='border pl-5'>Not a Member</td>
-        <td className='border pl-5' aria-hidden='true' />
-      </tr>
-    </>
+    <tr className={rows}>
+      <td className='border pl-5 pt-5 align-top'>{missionName}</td>
+      <td className='border p-5 text-justify'>{missionDescription}</td>
+      <td className='border pl-5'>Not a Member</td>
+      <td className='border pl-5' aria-hidden='true' />
+    </tr>
   );
 };
 
-export default Mission;
-
-Mission.PropTypes = {
+Mission.propTypes = {
   missionName: PropTypes.string.isRequired,
   missionDescription: PropTypes.string.isRequired,
   highlights: PropTypes.bool.isRequired,
 };
+
+export default Mission;
