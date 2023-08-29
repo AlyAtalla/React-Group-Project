014ead436b98c988a1fdf3aeac198a188406b98f
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-29 11:37:50 +0300

    Fix linter errors

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,4 +1,4 @@
-import React from 'react'; // Don't forget to import React
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -8,10 +8,10 @@ const Mission = ({ missionName, missionDescription, highlights }) => {
   });
   return (
     <tr className={rows}>
-      <td className='border pl-5 pt-5 align-top'>{missionName}</td>
-      <td className='border p-5 text-justify'>{missionDescription}</td>
-      <td className='border pl-5'>Not a Member</td>
-      <td className='border pl-5' aria-hidden='true' />
+      <td className="border pl-5 pt-5 align-top">{missionName}</td>
+      <td className="border p-5 text-justify">{missionDescription}</td>
+      <td className="border pl-5">Not a Member</td>
+      <td className="border pl-5" aria-hidden='true' />
     </tr>
   );
 };
