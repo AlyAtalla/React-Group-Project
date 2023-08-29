00cea106b28bb7f435d9c325acdb7cc3ffe20ca9
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-29 11:43:40 +0300

    Fix linter errors

--- src/components/MissionList.js
+++ src/components/MissionList.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMissions } from '../redux/missions/missionSlice';
-import Mission from './Mission';
+import Mission from './Mission.js';
 
 const MissionsList = () => {
   const { missions, isLoading } = useSelector((store) => store.missions);
@@ -12,17 +12,17 @@ const MissionsList = () => {
   }, [dispatch]);
 
   return (
-    <div className='px-[5rem] py-[2rem] font-manrope'>
+    <div className="px-[5rem] py-[2rem] font-manrope">
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <table className='border text-left'>
-          <thead className='h-16'>
+        <table className="border text-left">
+          <thead className="h-16">
             <tr>
-              <th className='border pl-5 w-1/6'>Mission</th>
-              <th className='border pl-5 w-3/6'>Description</th>
-              <th className='border pl-5 w-1/6'>Status</th>
-              <th className='border pl-5 w-1/6' aria-hidden='true' />
+              <th className="border pl-5 w-1/6">Mission</th>
+              <th className="border pl-5 w-3/6">Description</th>
+              <th className="border pl-5 w-1/6">Status</th>
+              <th className="border pl-5 w-1/6" aria-hidden="true" />
             </tr>
           </thead>
           <tbody>
