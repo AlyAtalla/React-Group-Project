23c945c1332d442058d135c64f194687e7db5b42
coker2019 <olabisi2k11@gmail.com>
2023-09-01 10:10:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
+import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import './App.css';
 import NavigationBar from './components/navigation-bar';
 import Rocket from './components/Rocket';
@@ -8,12 +9,31 @@ import Mission from './components/Missions';
 import MyProfile from './components/profile';
 
 import { getMissions } from './redux/missions/missionSlice';
+import { fetchRockets } from './redux/Rocket/rocketSlice';
 
 function App() {
+  const Url = 'https://api.spacexdata.com/v4/rockets';
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getMissions());
+  React.useEffect(() => {
+    axios
+      .get(Url)
+      .then((res) => {
+        dispatch(getMissions(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [dispatch]);
+  React.useEffect(() => {
+    axios
+      .get(Url)
+      .then((respond) => {
+        dispatch(fetchRockets(respond.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [dispatch]);
 
   return (
